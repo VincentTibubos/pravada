4babd8ee008891240bbdfcf79740bd2d96308117
David Boado <davidsgboado@gmail.com>
2018-07-03 19:49:55 +0800

    Login
    This is login

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
+app_name = 'app'
 urlpatterns = [
     path('',views.index, name='index'),
-    path('login/',views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('register/',views.register, name='register')
 ]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def index(request):
     return render(request, 'homepage/index.html')
 
 def login(request):
-    return render(request, 'account/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            return redirect ('app:home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'account/login.html',{ 'form':form })
 
 def register(request):
     return render(request, 'account/register.html')
+
+def home(request):
+    return render(request, 'account/home.html')
