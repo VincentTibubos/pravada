2935a1e39afb42c2ec99470c3c9ca0c7cdb10afc
David Boado <davidsgboado@gmail.com>
2018-07-06 23:27:09 +0800

    User Model
    Sparta

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=120, null=False, blank=False)
+    password = models.CharField(max_length=120, null=False, blank=False)
+    f_name = models.CharField(max_length=120, null=False, blank=False)
+    l_name = models.CharField(max_length=120, null=False, blank=False)
+    reputation = models.IntegerField(default=1, null=False, blank=False)
