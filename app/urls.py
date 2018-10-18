b02e5c90fbc9fd565192890415028295b661ab89
Kyle Aquino <kshaunaquino@gmail.com>
2018-10-18 10:20:06 +0800

    routes update account app

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+
+from . import views
 
 app_name = 'app'
 urlpatterns = [
